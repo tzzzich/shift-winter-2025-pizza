4a239682bce73547adbfa0d1d8208cf95c34ebbf
Viktoriya Bukharova <bvv005tkun@gmail.com>
2025-01-30 01:52:26 +0700

    feat(setup): setup testing component
    Created a simple custom color demonstration
    using added libraries' features.
    Were checked: tilewind, react-router-dom, ts

---
+++ src/pages/ColorSwatch.tsx
@@ -0,0 +1,47 @@
+import { ColorSwatchItem } from "@components";
+import { Card } from "@ui";
+
+const colors = [
+  {
+    color: "--color-bg-brand",
+    name: "BG/Brand",
+    hex: "#F4511E",
+  },
+  {
+    color: "--color-bg-pressed-primary",
+    name: "BG/Pressed Primary",
+    hex: "#E64A19",
+  },
+  {
+    color: "--color-bg-hover-primary",
+    name: "BG/Hover Primary",
+    hex: "#D84315",
+  },
+  {
+    color: "--color-bg-brand-extralight",
+    name: "BG/ExtraLight",
+    hex: "#FFAB91",
+  },
+  {
+    color: "--color-indicator-focused",
+    name: "Indicator/Focused",
+    hex: "#F4511E",
+  },
+];
+
+export default function ColorSwatchPage() {
+  return (
+    <div className="flex flex-col gap-5">
+      <Card className="flex flex-wrap justify-around bg-neutral-50 gap-[24px]">
+        {colors.map((color, index) => (
+          <ColorSwatchItem
+            key={index}
+            color={color.color}
+            name={color.name}
+            hex={color.hex}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
---
+++ src/shared/utils/constants/router.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  ROOT: "/",
+  LOGIN: "/login",
+  REGISTRATION: "/registration",
+};
